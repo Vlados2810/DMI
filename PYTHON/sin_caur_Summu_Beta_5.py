7b1e8bd7f9ed2a46f7eae7d9692df4095537dee8
Vlados2810 <31914198+Vlados2810@users.noreply.github.com>
2017-11-22 10:59:04 +0200

    Add files via upload

---
+++ PYTHON/sin_caur_Summu_Beta_5.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from math import sin
+# a0 a1 a2 a3 
+# S0 S1 S2 S3 -> S
+# a0 a1 a2 a3 -> 
+x = 1.*  input("Lietotāj, lūdzu, ievadi x argumentu (x): ")
+y = sin(x)
+
+print "sin(%6.2f) = %6.2f"%(x,y)
+k = 0
+a = (-1)**0*x**1/(1)
+S = a
+print "a0 = %6.2f S0 = %6.2f"% (a,S)
+
+
+while k <= 3:
+    k =k + 1 
+    a = a * (-1) * x**2/((2*k)*(2*k+1))
+    S = S + a
+    print "a%d = %6.2f S%d = %6.2f"% (k,a,k,S)
+print "Beigas!"
+
+'''
+k =k + 1 
+a = a * (-1) * x**2/((2*k)*(2*k+1))
+S = S + a
+
+print "a%d = %6.2f S%d = %6.2f"% (k,a,k,S)
+k = k + 1
+a = a * (-1) * x**2/((2*k)*(2*k+1))
+S = S +a
+
+print "a%d = %6.2f S%d = %6.2f"% (k,a,k,S)
+a = a * (-1)* x**2/((2*k)*(2*k+1))
+S = S +a 
+print "a%d = %6.2f S%d = %6.2f"% (k,a,k,S)
+'''
+
+
