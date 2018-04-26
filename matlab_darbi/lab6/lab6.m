3f766f713d9012706ae271c894d7c40976770c1b
user <v.abarovics@gmail.com>
2018-04-26 09:51:51 +0300

    lab6

---
+++ matlab_darbi/lab6/lab6.m
@@ -0,0 +1,18 @@
+%% Vidējās un efektīvas vērtības aprēķins
+%% Vidējās vērtības aprēķins
+t = 0:0.1:8;
+N = length(t);
+%%
+% * ar formulu 3a
+xvid3a = 1/(N-1)*sum(sig(t(1:end-1)))
+%%
+% *ar formulu 3a
+xvid3b = 1/(N-1)*sum(sig(t((1:end-1)+1)))
+%%
+% *ar formulu 3c
+h = (t(end)-t(1))/(N-1)
+xvid3c = 1/(N-1)*sum(sig(t(1:end-1)+h/2))
+%%
+% * ar formulu 4
+xvid4 = 1/(N-1)*(sig(t(1))/2 + sig(t(end))/2 + sum(sig(t(2:end-1))))
+
